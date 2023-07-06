cc8f645ece2c01afb224dcc46a644bee33f8cd9d
VincentKobz <vincent.libeskind@gmail.com>
2023-07-06 23:59:30 +0200

    feat: Add support of MQ135 sensor with serial print of data

--- weather_station/global_data.h
+++ weather_station/global_data.h
@@ -9,8 +9,17 @@
 #define MQTT_PORT 1883
 #define MQTT_DEVICE_ID "my_device"
 
+// MQ135
+#define BOARD ("ESP-32")
+#define VOLTAGE_RESOLUTION 3.3
+#define PIN 39
+#define TYPE "MQ-135"
+#define ADC_BIT_RESOLUTION 12
+#define RATIO_MQ135_CLEAN_AIR 3.6
+
 WiFiClient wifi_client;
 PubSubClient client(wifi_client);
+MQUnifiedsensor MQ135(BOARD, VOLTAGE_RESOLUTION, ADC_BIT_RESOLUTION, PIN, TYPE);
 
 static DHT dht(DHT_PIN, DHT_TYPE);
 
